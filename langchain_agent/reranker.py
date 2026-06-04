c98fe105109a9047477d51003cb1676ab9b594cf
Kevin Butler <kbutler@kmwllc.com>
2026-06-04 16:14:12 -0400

    Revert "fix: explicitly pass google_api_key to prevent ADC fallback with httpx header validation error"
    This reverts commit 8a524dfefb305aead070908945e9e81e79bab53a.

--- langchain_agent/reranker.py
+++ langchain_agent/reranker.py
@@ -14,7 +14,7 @@ from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel, Field, field_validator
 
-from config import GOOGLE_API_KEY, RERANKER_BATCH_SIZE
+from config import RERANKER_BATCH_SIZE
 from exceptions import RerankerLLMError, RerankerValidationError
 
 logger = logging.getLogger(__name__)
@@ -160,7 +160,6 @@ class GeminiReranker:
             temperature=0,
             streaming=False,
             max_output_tokens=2048,
-            google_api_key=GOOGLE_API_KEY,
         )
         self.structured_llm = self.llm.with_structured_output(RerankerScores)
 
